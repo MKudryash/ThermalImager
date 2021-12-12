6bc18fe42debe19d67e5d68a7a7d77cc154065b3
Julia Mamsheva <Jukudryash@gmail.com>
2021-12-12 17:59:05 +0300

    Добавьте файлы проекта.

---
+++ Spy/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Spy
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int column = StartWimdows.column;
+        static int row = StartWimdows.row;
+
+        double[] widthh = new double[column];
+        double[] heightt = new double[row];
+        struct User
+        {
+            public int x;
+            public int y;
+            public DateTime time;
+            public DateTime day;
+            public override string ToString()
+            {
+                return x + " " + y + " " + time + " " + day;
+            }
+        }
+        static List<User> users = new List<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            MessageBox.Show(column.ToString());
+            MessageBox.Show(row.ToString());
+            MessageBox.Show(StartWimdows.Ot.ToString());
+            MessageBox.Show(StartWimdows.Doo.ToString());
+            razmetka();
+            Hooks(StartWimdows.Ot,StartWimdows.Doo);
+        }
+
+
+        public void Hooks(DateTime ot, DateTime doo)
+        {
+            using (StreamReader sr = new StreamReader("D:Doc.csv"))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string[] str = sr.ReadLine().Split(';');
+
+                    users.Add(new User() { x = Convert.ToInt32(str[0]), y = Convert.ToInt32(str[1]), time = Convert.ToDateTime(str[2]), day = Convert.ToDateTime(str[3]) });
+                }
+            }
+            for (int i = 0; i < row; i++)
+            {
+
+                for (int j = 0; j < column; j++)
+                {
+                    int count = 0;
+                    for (int t = 0; t < users.Count; t++)
+                    {
+                        if (users[t].x < widthh[j] && users[t].y < heightt[i] && users[t].time >= ot && users[t].time <= doo&& users[i].day == StartWimdows.Search)
+                        {
+                            count++;
+                            users.RemoveAt(t);
+                        }
+
+                    }
+                    if (count > 2)
+                    {
+                        Rectangle myRect = new System.Windows.Shapes.Rectangle();
+                        Teplo.Children.Add(myRect);
+                        myRect.Fill = System.Windows.Media.Brushes.Green;
+                        Grid.SetColumn(myRect, j);
+                        Grid.SetRow(myRect, i);
+                    }
+                    if (count > 6)
+                    {
+                        Rectangle myRect = new System.Windows.Shapes.Rectangle();
+                        Teplo.Children.Add(myRect);
+                        myRect.Fill = System.Windows.Media.Brushes.Red;
+                        Grid.SetColumn(myRect, j);
+                        Grid.SetRow(myRect, i);
+                    }
+                }
+            }
+
+        }
+        public void razmetka()
+        {
+            double width = SystemParameters.PrimaryScreenWidth / column;
+            double height = SystemParameters.PrimaryScreenHeight / row;
+            for (int i = 0; i < column; i++)
+            {
+                widthh[i] = width * (i + 1);
+            }
+            for (int i = 0; i < row; i++)
+            {
+                heightt[i] = height * (i + 1);
+            }
+            Teplo.Children.Clear();
+            RowDefinition[] rows = new RowDefinition[row];
+            ColumnDefinition[] columns = new ColumnDefinition[column];
+            for (int i = 0; i < column; i++)
+            {
+                columns[i] = new ColumnDefinition();
+                Teplo.ColumnDefinitions.Add(columns[i]);
+                columns[i].Width = new GridLength(width);
+
+            }
+            for (int i = 0; i < row; i++)
+            {
+                rows[i] = new RowDefinition();
+                Teplo.RowDefinitions.Add(rows[i]);
+                rows[i].Height = new GridLength(height);
+            }
+
+        }
+    }
+}
---
+++ Spy/StartWimdows.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Spy
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWimdows.xaml
+    /// </summary>
+    public partial class StartWimdows : Window
+    {
+       public static int row;
+        public static int column;
+        public static DateTime Doo;
+        public static DateTime Ot;
+        public static DateTime Search;
+        public StartWimdows()
+        {
+            InitializeComponent();
+           
+        }
+
+
+        private void OpenTeplo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                column = Convert.ToInt32(txtColumn.Text);
+                row = Convert.ToInt32(txtRow.Text);
+                 Ot = Convert.ToDateTime(txttOt.Text);
+                 Doo  = Convert.ToDateTime(txtDo.Text);
+                Search = DateSearch.SelectedDate.Value;
+                MainWindow main = new MainWindow();
+                main.Show();
+                this.Close();
+            }
+            catch { }
+        }
+    }
+}
